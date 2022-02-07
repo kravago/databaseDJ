d2877fb29f20aebdb5437f93b52bc7d818a052ee
Engines-screaming <karl.ravago@gmail.com>
2022-02-06 23:03:51 -0800

    adding songs to playlist route is working

--- playlist-app/app.py
+++ playlist-app/app.py
@@ -116,23 +116,21 @@ def add_song_to_playlist(playlist_id):
     # Restrict form to songs not already on this playlist
 
     curr_on_playlist = [s.id for s in playlist.songs]
-    form.song.choices = (db.session.query(Song.id, Song.title)
-                        .filter(Song.id.notin_(curr_on_playlist))
-                        .all())
+    form.song.choices = [(row[0], row[1]) for row in (db.session.query(Song.id, Song.title)
+                                                            .filter(Song.id.notin_(curr_on_playlist))
+                                                            .all())
+    ]
+
+    # import pdb
+    # pdb.set_trace()
+
 
     if form.validate_on_submit():
 
-        # This is one way you could do this ...
         playlist_song = PlaylistSong(song_id=form.song.data,
                                     playlist_id=playlist_id)
         db.session.add(playlist_song)
 
-        # Here's another way you could that is slightly more ORM-ish:
-        #
-        # song = Song.query.get(form.song.data)
-        # playlist.songs.append(song)
-
-        # Either way, you have to commit:
         db.session.commit()
 
         return redirect(f"/playlists/{playlist_id}")
