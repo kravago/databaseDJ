e319505ef9ea11cd8f2e0615003159f14b929446
Engines-screaming <karl.ravago@gmail.com>
2022-02-06 16:00:21 -0800

    removed intercept redirects flag

--- playlist-app/app.py
+++ playlist-app/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///playlist-app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
 db.create_all()
--- playlist-app/models.py
+++ playlist-app/models.py
@@ -7,21 +7,33 @@ db = SQLAlchemy()
 
 class Playlist(db.Model):
     """Playlist."""
+    __tablename__ = 'playlist'
 
-    # ADD THE NECESSARY CODE HERE
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+
+    songs = db.relationship('Song', secondary='playlist_song', backref='playlist')
+    playlist_song = db.relationship('PlaylistSong', backref='playlist')
 
 
 class Song(db.Model):
     """Song."""
+    __tablename__ = 'song'
 
-    # ADD THE NECESSARY CODE HERE
-
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    artist = db.Column(db.String, nullable=False)
 
 class PlaylistSong(db.Model):
     """Mapping of a playlist to a song."""
+    __tablename__ = 'playlist_song'
 
-    # ADD THE NECESSARY CODE HERE
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'), nullable=False)
+    song_id = db.Column(db.Integer, db.ForeignKey('song.id'), nullable=False)
 
+    
 
 # DO NOT MODIFY THIS FUNCTION
 def connect_db(app):
